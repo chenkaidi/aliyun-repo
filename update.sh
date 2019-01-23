bfc911e42634c36458d6d431420fa8f40eebc26a
kaidi <991214519@qq.com>
2019-01-23 20:36:16 +0800

    Update update.sh

--- update.sh
+++ update.sh
@@ -8,10 +8,10 @@ yum -y install wget
 wget  http://mirrors.aliyun.com/repo/Centos-7.repo
 
 # 备份系统原来的repo文件
-mv  CentOs-Base.repo CentOs-Base.repo.bak
+mv  CentOS-Base.repo CentOS-Base.repo.bak
 
 #重命名 Centos-7.repo -> CentOs-Base.repo
-mv Centos-7.repo CentOs-Base.repo
+mv Centos-7.repo CentOS-Base.repo
 
 #执行yum源更新命令
 yum clean all
